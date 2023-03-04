a66b8ca296bae5259cedb40dfdabdf3d9b8a2b4f
maestroerror <revaz.gh@gmail.com>
2023-03-04 04:22:01 +0400

    Added PEST test

---
+++ tests/Unit/ConvertationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+include "src/HeicToJpg.php";
+use Maestroerror\HeicToJpg;
+
+test('Converts and saves to the file', function () {
+    $resultImg = "saved-as-jpg.jpg";
+    // Save converted image
+    HeicToJpg::convert("image1.heic")->saveAs($resultImg);
+    // Check mime type
+    $mime = mime_content_type($resultImg);
+    // Check image exists
+    expect(file_exists($resultImg))->toBeTrue();
+    // Check image is JPEG
+    expect($mime == "image/jpeg")->toBeTrue();
+    // Remove image
+    unlink($resultImg);
+});
+
+
+test('Converts and gives content', function () {
+    $resultImg = "saved-with-php.jpg";
+    // Get content of converted image
+    $jpg = HeicToJpg::convert("image1.heic")->get($resultImg);
+    // Save image
+    file_put_contents($resultImg, $jpg);
+    // Get mime type
+    $mime = mime_content_type($resultImg);
+    // Check image exists
+    expect(file_exists($resultImg))->toBeTrue();
+    // Check image is JPEG
+    expect($mime == "image/jpeg")->toBeTrue();
+    // Remove image
+    unlink($resultImg);
+});
