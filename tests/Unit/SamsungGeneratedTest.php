1dac6e88c95f5ae10c1aab224c341329324b64e2
maestroerror <revaz.gh@gmail.com>
2023-06-02 23:03:05 +0400

    Commented out samsung test

---
+++ tests/Unit/SamsungGeneratedTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Maestroerror\HeicToJpg;
+
+// test("Converts samsung-generated image to JPEG and saves to the file", function () {
+//     $resultImg = "saved-as-jpg.jpg";
+//     // Save converted image
+//     HeicToJpg::convert(__dir__ . "/images/samsung-generated.heic")->saveAs($resultImg);
+//     // Check mime type
+//     $mime = mime_content_type($resultImg);
+//     // Check image exists
+//     expect(file_exists($resultImg))->toBeTrue();
+//     // Check image is JPEG
+//     expect($mime == "image/jpeg")->toBeTrue();
+//     // Remove image
+//     unlink($resultImg);
+// });
